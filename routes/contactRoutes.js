186e2b56e79c8d07958baaf5f999eed60e4fe92e
codexsuvro <suvrodeepdebnath.official@gmail.com>
2024-11-28 23:12:32 +0530

    Adding comment line

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+// To get all the contacts
 router.route("/").get((req, res) => {
     res.status(200).json({message: "Get all contacts"});
 });
 
+// To get individual contact
 router.route("/:id").get((req, res) => {
     res.status(200).json({message: `Get contact | ${req.params.id}`});
 });
 
+// To create new contact
 router.route("/").post((req, res) => {
     res.status(200).json({message: "Create contact"});
 });
 
+// To update contact
 router.route("/:id").put((req, res) => {
     res.status(200).json({message: `Update contact | ${req.params.id}`});
 });
 
+// To delete contact
 router.route("/:id").delete((req, res) => {
     res.status(200).json({message: `Delete contact | ${req.params.id}`});
 });
