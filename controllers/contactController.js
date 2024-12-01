5a5e36977ae2d67d93c4ca64ed88ae116f29dee1
Suvrodeep Debnath <suvrodeepdebnath.official@gmail.com>
2024-12-01 19:27:09 +0530

    Implement contact retrieval, update, and deletion with error handling

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -17,7 +17,13 @@ const getContacts = asyncHandler(async (req, res) => {
 // @access public
 
 const getContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Get contact | ${req.params.id}` });
+    // Finding the contact by the id
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
 });
 
 // @description Create contact
@@ -45,7 +51,18 @@ const createContact = asyncHandler(async (req, res) => {
 // @access public
 
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update contact | ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        // Passing the query to flag itself as new one
+        { new: true },
+    );
+    res.status(200).json(updatedContact);
 });
 
 // @description Delete contact
@@ -53,7 +70,13 @@ const updateContact = asyncHandler(async (req, res) => {
 // @access public
 
 const deleteContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete contact | ${req.params.id}` });
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    await Contact.findByIdAndDelete(req.params.id);
+    res.status(200).json(contact);
 });
 
 module.exports = { getContacts, getContact, createContact, updateContact, deleteContact };
